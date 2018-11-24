8ab3b802871f01d7ddf1033d921644e39b93e9e7
Nroot_SW_33 <44187289+Nroot33@users.noreply.github.com>
2018-11-24 19:30:04 +0900

    init

---
+++ 11_public.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.24;
+
+contract MyName {
+    
+    string myName; 
+
+   
+    function getMyName() constant public returns(string) {
+        return myName;
+    }
+
+  
+    function setMyName(string newMyName) public {
+        getMyName();
+    }
+}
+
+contract CallContract is MyName {
+    function callGetMyName() public {
+        getMyName();
+    }
+}
+
+contract CallContract2 {
+    MyName myName;
+    function callGetMyName() public {
+        myName.getMyName();
+    }
+}
