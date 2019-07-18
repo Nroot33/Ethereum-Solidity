0dc91da4c30f9799200adabbbe2af223e2018567
Nroot_SW_33 <jessica0533@gmail.com>
2019-07-18 14:10:15 +0900

    Create Test_Vote.sol
    Create Test_Vote.sol

---
+++ Test_Vote.sol
@@ -0,0 +1,70 @@
+pragma solidity 0.4.24;
+
+contract Vote{
+
+    //structure
+    struct candidator{
+        string name;
+        uint upVote;
+    }
+    
+    //variable
+    bool live;
+    address owner;
+    candidator[] public candidatorlist;
+    
+    
+    //mapping
+    mapping(address => bool) Voted;
+    
+    
+    //event
+    event AddCandiator(string name);
+    event UpVote(string candidator, uint upVote);
+    event FinishVote(bool live);
+    event Voting(address owner)
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    //constructor
+    constructor() public{
+        owner = msg.sender;
+        live =true;
+        
+        emit Voting(owner);
+    }
+    
+    
+    // candidator
+    function addCandiator(string _name) onlyOwner{
+        require(live == true);
+        require(candidatorlist.length < 3);
+        candidatorlist.push(candidator(_name,0));
+        
+        emit AddCandiator(_name);
+    }
+    
+    // voting
+    function upVote(uint _indexofCandiator) public{
+        require(live == true);
+        require(_indexofCandiator < candidatorlist.length);
+        require(Voted[msg.sender] == false);
+        
+        candidatorlist[_indexofCandiator].upVote++;    
+        
+        Voted[msg.sender] = true;
+        
+        emit UpVote(candidatorlist[_indexofCandiator].name, candidatorlist[_indexofCandiator].upVote);
+    }
+    
+    // finish vote
+    function FinishVote() onlyOwner{
+        require(live == true);
+        live = false;
+        
+        emit FinishVote(live);
+    }
+}
